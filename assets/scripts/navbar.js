f139242f978f2097d3137fc038eae29fdd5d104a
martinfruehauf <martin.fruehauf@gmail.com>
2020-11-30 19:29:06 +0100

    Remove debugging logs

--- assets/scripts/navbar.js
+++ assets/scripts/navbar.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
     $(".container").load("home.html");
-    console.log($("ul.navbar-nav li a").attr("data-page"));
 });
 
 $("ul.navbar-nav li a").each(function() {
     $(this).on("click", function(){
-        console.log($(this).attr("data-page"));
         $(".container").load($(this).attr("data-page")+".html");
     });
 });
