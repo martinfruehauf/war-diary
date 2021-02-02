cf1147e435ae70231cfd808568c8c95a6038251a
martinfruehauf <martin.fruehauf@gmail.com>
2021-02-02 16:43:07 +0100

    Change center of map

--- assets/scripts/map.js
+++ assets/scripts/map.js
@@ -4,7 +4,7 @@ let markers = [];
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
         zoom: 6,
-        center: { lat: 52.895563, lng: 17.116324 },
+        center: { lat: 53.3, lng: 17.116324 },
         mapTypeId: "terrain",
     });
 
